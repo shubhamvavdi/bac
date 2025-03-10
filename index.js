954b4df7552f72514b9932c020b41c20a2a8c837
code28nodeandreact <csmshubham@gmail.com>
2025-03-10 18:30:05 +0530

    ok

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 // ✅ MongoDB Connection
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/bookstore";
+
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB Connected"))
